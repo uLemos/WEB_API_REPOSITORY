974870ed16f819000a2fcdaf5f09395786a26220
Fernando Lemos <fsnvag12@gmail.com>
2023-06-12 19:13:01 -0300

    WEB API REPOSITORY

---
+++ Context.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Dominio;
+
+namespace WebAPI.Repository
+{
+    public class Context : IdentityDbContext<User, Role, int, //Quanto o meu contexto estiver rodando, quero todos como inteiro.
+                                                IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+                                                IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public Context(DbContextOptions<Context> options) : base(options){ }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role) //Role
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User) //User
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
+            builder.Entity<Organizacao>(org =>
+            {
+                org.ToTable("Organizacoes");
+                org.HasKey(x => x.Id);
+
+                org.HasMany<User>()
+                    .WithOne()
+                    .HasForeignKey(x => x.OrgId)
+                    .IsRequired(false);
+            });
+        }
+    }
+}
